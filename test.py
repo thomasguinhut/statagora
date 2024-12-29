8a0a4befe3e5ea6c47a2ba0a4b5ee87ba13b0ea6
Thomas Guinhut <151759734+thomasguinhut@users.noreply.github.com>
2024-12-29 16:39:03 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import toml
-
-try:
-    with open("/Users/thomasguinhut/Documents/statagora/.streamlit/secrets.toml", "r") as file:
-        data = toml.load(file)
-    print("Le fichier TOML est valide.")
-except Exception as e:
-    print(f"Erreur dans le fichier TOML : {e}")
