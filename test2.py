ce4135dc82d739535c7035d5bc48ca982a807a39
Thomas Guinhut <151759734+thomasguinhut@users.noreply.github.com>
2025-01-04 13:26:37 +0100

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-from src.dao.db_connection import DBConnection
-from src.dao.reset_database import ResetDatabase
-from src.client.ssmsi_client import SsmsiClient
-from src.client.dares_client import DaresClient
-from src.service.publication_service import PublicationService
-
-print(DBConnection().afficher_df())
